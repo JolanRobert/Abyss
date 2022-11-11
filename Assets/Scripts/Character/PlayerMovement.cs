dad5e109a1782d618ac971a7b3a733482e55bc03
Xavier L <xavier.lezzoche@gmail.com>
2022-11-11 11:25:14 +0100

    Player Movement

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -5,9 +5,26 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D rb2d;
     [SerializeField] private PlayerController playerController;
     private PlayerData data => playerController.data;
-    
+
+    private float timer = 0;
+
     public void Move(float moveInput)
     {
+        if (rb2d.velocity.x >= 0 && moveInput > 0)
+        {
+            timer += Time.deltaTime;
+            rb2d.velocity = new Vector3(data.moveCurve.Evaluate(timer), 0);
+        }
+        else if (rb2d.velocity.x <= 0 && moveInput < 0)
+        {
+            timer += Time.deltaTime;
+            rb2d.velocity = new Vector3(-data.moveCurve.Evaluate(timer), 0);
+        }
+        else 
+        {
+            timer = 0;
+            rb2d.velocity = Vector2.zero;
+        }
         
     }
 }
