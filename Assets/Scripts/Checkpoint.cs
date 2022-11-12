4d383edd1fe173c57f9aed3f15c1b1bbc2b692f1
Jolan <jolan.robert56@gmail.com>
2022-11-12 13:46:07 +0100

    Refactor name and file location

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -1,14 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckPoint : MonoBehaviour
+public class Checkpoint : MonoBehaviour
 {
 
     private PlayerController playerController;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.name);
         playerController = collision.GetComponent<PlayerController>();
         if (playerController != null) 
         {
